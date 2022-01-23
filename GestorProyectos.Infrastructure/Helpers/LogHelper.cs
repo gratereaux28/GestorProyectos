1c22f5f30dff2c48672dc4250e14a9c4cfab789b
Martin Gratereaux Peña <gratereaux28@hotmail.com>
2022-01-23 16:30:43 -0400

    LogerHelper

--- GestorProyectos.Infrastructure/Helpers/LogHelper.cs
+++ GestorProyectos.Infrastructure/Helpers/LogHelper.cs
@@ -15,12 +15,12 @@ namespace GestorProyectos.Infrastructure.Helpers
         public static void Write(string ClassName, int Line, string user, string Mensaje)
         {
             string Path = UrlLog + "/" + LogDate;
-            string LogFile = ($@"{Path}\Logs {LogDate}  ({user}).txt");
+            string LogFile = ($@"{Path}\Logs {LogDate}.txt");
 
             Directory.CreateDirectory(Path);
             using (StreamWriter w = new StreamWriter(LogFile, true))
             {
-                w.WriteLine(LogTime + $" Error en la clase: ({ClassName}) en Linea: ({Line})  ===> " + Mensaje);
+                w.WriteLine(LogTime + $" Error en la clase: ({ClassName}) en Linea: ({Line}) por el Usuario: ({user}) ===> " + Mensaje);
                 w.Flush();
                 w.Close();
             }
@@ -37,15 +37,15 @@ namespace GestorProyectos.Infrastructure.Helpers
 
         public static void Write(string ClassName, string user, Exception ex)
         {
-            string Path = UrlLog + "/" + LogDate;
-            string LogFile = ($@"{Path}\Logs {LogDate} ({user}).txt");
+            string Path = UrlLog;
+            string LogFile = ($@"{Path}\Logs {LogDate}.txt");
             string Mensaje = ex.Message;
             int Line = 0;
             int.TryParse(ex.StackTrace.Substring(ex.StackTrace.LastIndexOf(' ')), out Line);
             Directory.CreateDirectory(Path);
             using (StreamWriter w = new StreamWriter(LogFile, true))
             {
-                w.WriteLine(LogTime + $" Error en la clase: ({ClassName}) en Linea: ({Line})  ===> " + Mensaje);
+                w.WriteLine(LogTime + $" Error en la clase: ({ClassName}) en Linea: ({Line}) por el Usuario: ({user}) ===> " + Mensaje);
                 w.Flush();
                 w.Close();
             }
